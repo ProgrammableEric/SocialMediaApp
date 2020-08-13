bb4b0ced82d8c54d0a11dffe0188f4c74e2705fd
Eric Fu <u5885811@anu.edu.au>
2020-08-13 23:57:56 +1000

    trying to fetch and display screams

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Components
 import Navbar from './components/Navbar';
@@ -10,13 +12,34 @@ import home from './pages/home';
 import login from './pages/login';
 import signUp from './pages/signUp';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
 // react router does partial matching, giving '/' it'll return the home page 
 // rather than continuing searching for what comes next. (such as '/login'). 
 // use the exact keyword to disable partial matching. 
 
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
       <Router>
         <Navbar />
         <div className="container">
@@ -27,7 +50,8 @@ function App() {
           </Switch>
         </div>
       </Router>
-    </div>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
